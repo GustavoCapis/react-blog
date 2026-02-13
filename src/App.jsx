99dcb192458646aab4ec315521aec428f1e06221
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-02-13 16:20:37 -0300

    feat: Add state and effect to fetch from the api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import api from "./api/posts";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (error) {
+        if (error.response) {
+          //Not in the 200 response range
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else {
+          //No response or 404 error
+          console.log(`Error: ${error.message}`);
+        }
+      }
+    };
+    fetchPosts();
+  }, []);
+  console.log(posts);
   return (
     <>
       <Header />
+      <section></section>
       <Footer />
     </>
   );
