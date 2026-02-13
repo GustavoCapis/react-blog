cab56ebfdfbec95f9c4ea84a0d492a2320b5be00
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-02-13 16:02:18 -0300

    feat: Setup json server and import axios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import api from "./api/posts";
 
 function App() {
   return (
     <>
       <Header />
-      <Footer/>
+      <Footer />
     </>
   );
 }
