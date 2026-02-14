946db280538d1b5531fa370dfa4a04c57a853bd8
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-02-14 20:10:07 -0300

    feat: Add handle submit function to post.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 import api from "./api/posts";
 import Header from "./Header";
 import Home from "./Home";
@@ -7,10 +8,12 @@ import Post from "./Post";
 import Footer from "./Footer";
 
 function App() {
+  //Redirect to home after submit
+  const navigate = useNavigate();
   //States
   const [posts, setPosts] = useState([]);
-  const [postTitle, setPostTitle] = useState("")
-  const [postBody, setPostBody] = useState("")
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
 
   //Fetch request
   useEffect(() => {
@@ -25,30 +28,50 @@ function App() {
           console.log(error.response.status);
           console.log(error.response.headers);
         } else {
-          //No response 
+          //No response
           console.log(`Error: ${error.message}`);
         }
       }
     };
     fetchPosts();
   }, []);
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
+
+  //On form submit
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = crypto.randomUUID();
+    const date = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      date,
+      body: postBody,
+    };
+    setPosts((prev) => [...prev, newPost]);
+    //Clear form after submit
+    setPostTitle("");
+    setPostBody("");
+    //Redirect to home after submit
+    navigate("/");
   }
 
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home posts={posts} />}/>
-        <Route path="/post" 
-        element={<Post 
-        postTitle={postTitle} 
-        postBody={postBody}
-        setPostTitle={setPostTitle}
-        setPostBody={setPostBody}
-        />}></Route>
+        <Route path="/" element={<Home posts={posts} />} />
+        <Route
+          path="/post"
+          element={
+            <Post
+              postTitle={postTitle}
+              postBody={postBody}
+              setPostTitle={setPostTitle}
+              setPostBody={setPostBody}
+              handleSubmit={handleSubmit}
+            />
+          }
+        ></Route>
       </Routes>
       <Footer />
     </>
--- src/Post.jsx
+++ src/Post.jsx
@@ -2,20 +2,22 @@ export default function Post(props) {
   return (
     <section className="new-post-section">
       <h2>New Post</h2>
-      <form action="">
+      <form onSubmit={props.handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input 
-        type="text" 
-        name="title" 
-        id="title" 
-        value={props.postTitle}
-        onChange={(e)=> props.setPostTitle(e.target.value)}/>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={props.postTitle}
+          onChange={(e) => props.setPostTitle(e.target.value)}
+        />
         <label htmlFor="body">Post:</label>
-        <textarea 
-        value={props.postBody}
-        onChange={(e)=>props.setPostBody(e.target.value)}
-        name="body" 
-        id="body"></textarea>
+        <textarea
+          value={props.postBody}
+          onChange={(e) => props.setPostBody(e.target.value)}
+          name="body"
+          id="body"
+        ></textarea>
         <button type="submit">Submit</button>
       </form>
     </section>
