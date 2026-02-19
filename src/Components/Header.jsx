774a5d6d6d313e8e3578e8d1627034165c6e14e7
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-02-19 15:37:16 -0300

    fix: correct logo import path.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import Navbar from "./Navbar";
 
 export default function Header({ searchPost, onSearchChange }) {
